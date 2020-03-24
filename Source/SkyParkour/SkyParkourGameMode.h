e39c4420d4eb883596d2f670cb4f6702a3d68791
Luca Martinelli <lucxmangajet@gmail.com>
2020-03-24 11:19:31 +0100

    first commit

---
+++ Source/SkyParkour/SkyParkourGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkyParkourGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASkyParkourGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASkyParkourGameMode();
+};
+
+
+
